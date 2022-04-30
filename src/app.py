1f381143e95f18e8063621b64b119fe7fb5bf60b
juandinetUPC <juandinet@unicesar.edu.co>
2022-04-29 22:31:09 -0500

    RC con test unitarios

--- src/app.py
+++ src/app.py
@@ -5,68 +5,77 @@ import pandas as pd
 import hashlib
 from sqlalchemy import create_engine
 # import locale
-
+df=pd.DataFrame(columns=["Region", "Country Name","Languaje", "Time(ms)"])
+engine=create_engine('sqlite://', echo=False)
 # locale.setlocale(locale.LC_ALL, '')
 
 
 
-url='https://restcountries.com/v2/name/'
-formato='?fullText=true'
 
-listadePaises = ["Angola","Argentina", "Brasil", "Chile", "Colombia", "Ecuador", "Paraguay", "Uruguay", "Venezuela", "españa","Valledupar"]
+
+
 
 def obtenerDatosdelPais(pais):
     """Dado el nombre de un pais, devuelve una lista con los datos del pais
     de la API de RestCountries"""
-    url_pais = url + pais.lower().strip() #+ formato
+    url='https://restcountries.com/v2/name/'
+    url_pais = url + pais.lower().strip() # + '?fullText=true'
     r = requests.get(url_pais)  # r es un objeto de la clase Response
     if r.status_code == 200:
         return r.json()
     else:
         return None
+
 def generarFila(pais):
         startTime=time.time()
         datosPais = obtenerDatosdelPais(pais)
         if datosPais is not None:
-            # print(datosPais[0]['region'])
-            # print(datosPais[0]['name'])
             idioma=datosPais[0]['languages'][0]['name']
-            # print(idioma)
             hash_idioma = hashlib.sha1(b'{idioma}')
-            # print(hash_idioma.hexdigest())
             endTime=time.time()
             tiempo=(endTime-startTime)*1000
-
             # fila = {"Region":datosPais[0]['region'], "Country Name":datosPais[0]['name'],"Languaje":hash_idioma.hexdigest(), "Time":tiempo}
             fila=[datosPais[0]['region'],datosPais[0]['name'],hash_idioma.hexdigest(),tiempo]
         else:
             endTime=time.time()
             tiempo=(endTime-startTime)*1000
-            # fila = {"Region":"None", "Country Name":pais,"Languaje":"No se encontraron datos", "Time":0}
-            fila=["",pais,"No se encontraron datos",tiempo]           
+            # fila=["",pais,"No se encontraron datos",tiempo]           
+            fila=None           
         return fila
 
+def adicionarFila(pais,df):
+    '''Usando la funcion generarFilas(pais) Agrega una fila al dataframe df
+    , guarda el dataframe en la base de datos y en un archivo .json'''
+    fila = generarFila(pais)
+    if fila!=None:
+        df.loc[len(df)]=fila
+        # Guardar en una base de datos 
+        df.to_sql('paises', engine, if_exists='replace')
+        # Guardar en un json
+        df.to_json('data.json')
+    else:
+        print("No se encontraron datos para el pais: ", pais)
+    return df
+    
+    
+
 if __name__=='__main__':
-    indiceFilas=0
-    df=pd.DataFrame(columns=["Region", "Country Name","Languaje", "Time(ms)"])
+    listadePaises = ["Angola","Argentina", "Brasil", "Chile", "Colombia", "Ecuador", "Paraguay", "Uruguay", "Venezuela", "España","Valledupar"]
     for pais in listadePaises:
-        df.loc[indiceFilas]=generarFila(pais)
-        # df = df.append(generarFila(pais),ignore_index=True)
-        indiceFilas+=1
+        adicionarFila(pais,df)
     
+    print('_____________________________________________________________')
     print(df)
-    
+  
     print("Tiempo total: ",df["Time(ms)"].sum(), "ms")    
     print("Tiempo promedio: ",df["Time(ms)"].mean(), "ms")    
     print("Tiempo Mínimo: ",df["Time(ms)"].min(), "ms")
     print("Tiempo Máximo: ",df["Time(ms)"].max(), "ms")
 
-    # Guardar en una base de datos 
-    engine=create_engine('sqlite://', echo=False)  
-    df.to_sql('paises', engine, if_exists='replace')
     
-    # Guardar en un json
-    df.to_json('data.json')
     
-    #print(engine.execute("SELECT * FROM paises").fetchall())
+    
+    
+    # Consulta a la base de datos para probar que se guardaron los datos
+    print(engine.execute("SELECT * FROM paises").fetchall())
     
---
+++ src/test_app.py
@@ -0,0 +1,70 @@
+import app
+import unittest
+import pandas as pd
+from sqlalchemy import create_engine
+df=pd.DataFrame(columns=["Region", "Country Name","Languaje", "Time(ms)"])
+# engine=create_engine('sqlite://', echo=False)
+
+class TestApp(unittest.TestCase):
+    def test_obtenerDatosdelPais_Ang(self):
+        pais = 'Angola'
+        datosPais = app.obtenerDatosdelPais(pais)
+        self.assertEqual(datosPais[0]['region'], 'Africa')
+        self.assertEqual(datosPais[0]['name'], 'Angola')
+        self.assertEqual(datosPais[0]['languages'][0]['name'], 'Portuguese')
+
+    def test_obtenerDatosdelPais_Esp(self):
+        pais = 'España'
+        datosPais = app.obtenerDatosdelPais(pais)
+        self.assertEqual(datosPais[0]['region'], 'Europe')
+        self.assertEqual(datosPais[0]['name'], 'Spain')
+        self.assertEqual(datosPais[0]['languages'][0]['name'], 'Spanish')
+
+    def test_obtenerDatosdelPais_Col(self):
+        pais = 'Colombia'
+        datosPais = app.obtenerDatosdelPais(pais)
+        self.assertEqual(datosPais[0]['region'], 'Americas')
+        self.assertEqual(datosPais[0]['name'], 'Colombia')
+        self.assertEqual(datosPais[0]['languages'][0]['name'], 'Spanish')
+
+    def test_obtenerDatosdelPais_Vall(self):
+        pais = 'Valledupar'
+        datosPais = app.obtenerDatosdelPais(pais)
+        self.assertEqual(datosPais, None)
+
+    def test_generarFila_Esp(self):
+        pais = 'España'
+        fila = app.generarFila(pais)
+        self.assertEqual(fila[0], 'Europe')
+        self.assertEqual(fila[1], 'Spain')
+        # self.assertEqual(fila[2], 'sha1$5f47a1b3$1$3c3e3a8a2b9d2a7a9a9b9c1b076b0d6b1c6e5e0f')
+
+    def test_generarFila_Col(self):
+        pais = 'Colombia'
+        fila = app.generarFila(pais)
+        self.assertEqual(fila[0], 'Americas')
+        self.assertEqual(fila[1], 'Colombia')
+        # self.assertEqual(fila[2], 'sha1$5f47a1b3$1$3c3e3a8a2b9d2a7a9a9b9c1b076b0d6b1c6e5e0f')    
+    
+    
+    def test_generarFila_Ang(self):
+        pais = 'Angola'
+        fila = app.generarFila(pais)
+        self.assertEqual(fila[0], 'Africa')
+        self.assertEqual(fila[1], 'Angola')
+        # self.assertEqual(fila[2], 'sha1$5f47a1b3$1$3c3e3a8a2b9d2a7a9a9b9c1b076b0d6b1c6e5e0f')
+
+    def test_generarFila_Valle(self):
+        pais = 'Valledupar'
+        fila = app.generarFila(pais)
+        self.assertEqual(fila, None)
+    
+    def test_adicionarFila(self):
+        listadePaises = ["Angola","Argentina", "Brasil", "Chile", "Colombia", "Ecuador", "Paraguay", "Uruguay", "Venezuela", "España","Valledupar"]
+        for pais in listadePaises:
+            app.adicionarFila(pais, df)
+        self.assertEqual(df["Region"][0], 'Africa')
+        self.assertEqual(df["Region"][len(df)-1], 'Europe')
+            
+    
+    
