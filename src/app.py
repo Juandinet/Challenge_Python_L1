a93a6489ec125bf54861c2182c3679a714e3ff30
juandinetUPC <juandinet@unicesar.edu.co>
2022-05-02 20:13:19 -0500

    Argumentos por consola

--- src/app.py
+++ src/app.py
@@ -3,22 +3,31 @@ import requests
 import pandas as pd
 import hashlib
 from sqlalchemy import create_engine
+import sys
 df=pd.DataFrame(columns=["Region", "Country Name","Languaje", "Time(ms)"])
 engine=create_engine('sqlite://', echo=False)
 
 def obtenerDatosdelPais(pais):
     """Dado el nombre de un pais, devuelve una lista con los datos del pais
-    de la API de RestCountries"""
-    url='https://restcountries.com/v2/name/'
-    url_pais = url + pais.lower().strip() # + '?fullText=true'
-    r = requests.get(url_pais)  # r es un objeto de la clase Response
-    if r.status_code == 200:
-        return r.json()
-    else:
-        return None
+    de la API de restCountries.com"""
+    try:
+        url = "https://restcountries.com/v2/name/" + pais
+        respuesta = requests.get(url)
+        
+        if respuesta.status_code == 200:
+            return respuesta.json()
+            
+        else:
+            return None
+        
+    except:
+        print('la url no responde')
+        exit()
+        
+    
 
 def generarFila(pais):
-    """Dado el nombre de un pais, devuelve una lista con los datos del pais"""
+    """Dado el nombre de un pais, devuelve una fila(lista) con los datos que requerimos del pais"""
     startTime=time.time()
     datosPais = obtenerDatosdelPais(pais)
     if datosPais is not None:
@@ -26,12 +35,11 @@ def generarFila(pais):
         hash_idioma = hashlib.sha1(idioma.encode('utf-8')).hexdigest()
         endTime=time.time()
         tiempo=(endTime-startTime)*1000
-        # fila = {"Region":datosPais[0]['region'], "Country Name":datosPais[0]['name'],"Languaje":hash_idioma.hexdigest(), "Time":tiempo}
         fila=[datosPais[0]['region'],datosPais[0]['name'],hash_idioma,tiempo]
     else:
         endTime=time.time()
         tiempo=(endTime-startTime)*1000
-        # fila=["",pais,"No se encontraron datos",tiempo]           
+        print(f"No se encontraron datos del pais {pais}, tiempo de búsqueda {tiempo}ms")           
         fila=None           
     return fila
 
@@ -42,32 +50,37 @@ def adicionarFila(pais,df):
     if fila!=None:
         df.loc[len(df)]=fila
         
-    else:
-        print("No se encontraron datos para el pais: ", pais)
+    # else:
+    #    print("No se encontraron datos para el pais: ", pais)
     return df
     
     
 
 if __name__=='__main__':
-    listadePaises = ["Angola","Argentina", "Brasil", "Chile", "Colombia", "United States of America", "United K", "Uruguay", "Venezuela", "España","Valledupar","Bogota"]
-    for pais in listadePaises:
-        adicionarFila(pais,df)
+    # listadePaises = ["Angola","Argentina", "Brasil", "Chile", "Colombia", "United States of America", "United K", "Uruguay", "Venezuela", "España","Valledupar","Bogota"]
+    if len(sys.argv)>1:
+        listadePaises=sys.argv[1:]
+        # print(listadePaises)
+        for pais in listadePaises:
+            df=adicionarFila(pais,df)
+            df.to_sql('paises',engine,if_exists='replace',index=False)
+            df.to_json('paises.json',orient='records')
     
-    print('_____________________________________________________________')
-    print(df)
-  
-    print("Tiempo total: ",df["Time(ms)"].sum(), "ms")    
-    print("Tiempo promedio: ",df["Time(ms)"].mean(), "ms")    
-    print("Tiempo Mínimo: ",df["Time(ms)"].min(), "ms")
-    print("Tiempo Máximo: ",df["Time(ms)"].max(), "ms")
+        print('_____________________________________________________________')
 
-    # Guardar DataFrame en una base de datos 
-    df.to_sql('paises', engine, if_exists='replace')
-    # Guardar DataFrame en un json
-    df.to_json('data.json')
+        print(df)
     
-    
-    
-    # Consulta a la base de datos para probar que se guardaron los datos
-    print(engine.execute("SELECT * FROM paises").fetchall())
-    
+        print("Tiempo total: ",df["Time(ms)"].sum(), "ms")    
+        print("Tiempo promedio: ",df["Time(ms)"].mean(), "ms")    
+        print("Tiempo Mínimo: ",df["Time(ms)"].min(), "ms")
+        print("Tiempo Máximo: ",df["Time(ms)"].max(), "ms")
+
+        # Guardar DataFrame en una base de datos 
+        df.to_sql('paises', engine, if_exists='replace')
+        # Guardar DataFrame en un json
+        df.to_json('data.json')
+        
+        # Consulta a la base de datos para probar que se guardaron los datos
+        print(engine.execute("SELECT * FROM paises").fetchall())
+    else:
+        print("No se ingresaron paises")    
