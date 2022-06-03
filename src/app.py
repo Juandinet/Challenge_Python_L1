f75732f6ffd13535de547f76a450370d0b4464ce
Juandinet <juandinet@gmail.com>
2022-06-03 10:08:29 -0500

    Argumentos por consola

--- src/app.py
+++ src/app.py
@@ -4,65 +4,60 @@ import pandas as pd
 import hashlib
 from sqlalchemy import create_engine
 import sys
-df=pd.DataFrame(columns=["Region", "Country Name","Languaje", "Time(ms)"])
-engine=create_engine('sqlite://', echo=False)
+df = pd.DataFrame( columns = ["Region", "Country Name","Languaje", "Time(ms)"] )
+engine = create_engine( 'sqlite://', echo = False )
 
-def obtenerDatosdelPais(pais):
-    """Dado el nombre de un pais, devuelve una lista con los datos del pais
-    de la API de restCountries.com"""
+def obtian_DataCuntry( country ):
+    '''Dado un pais, los cunsulta  en restcountries.com y devuelve una lista con los datos del pais'''
     try:
-        url = "https://restcountries.com/v2/name/" + pais
-        respuesta = requests.get(url)
+        url = "https://restcountries.com/v2/name/" + country
+        response = requests.get( url )
         
-        if respuesta.status_code == 200:
-            return respuesta.json()
+        if response.status_code == 200:
+            return response.json()
             
         else:
             return None
         
     except:
-        print('la url no responde')
+        print( 'la url no responde' )
         exit()
         
     
 
-def generarFila(pais):
+def generate_countryRow( country ):
     """Dado el nombre de un pais, devuelve una fila(lista) con los datos que requerimos del pais"""
-    startTime=time.time()
-    datosPais = obtenerDatosdelPais(pais)
-    if datosPais is not None:
-        idioma=datosPais[0]['languages'][0]['name']
-        hash_idioma = hashlib.sha1(idioma.encode('utf-8')).hexdigest()
-        endTime=time.time()
-        tiempo=(endTime-startTime)*1000
-        fila=[datosPais[0]['region'],datosPais[0]['name'],hash_idioma,tiempo]
+    startTime = time.time()
+    dataCountry = obtian_DataCuntry( country )
+    if dataCountry is not None:
+        country_lang = dataCountry[0]['languages'][0]['name']
+        hash_country_lang = hashlib.sha1(country_lang.encode( 'utf-8' ) ).hexdigest()
+        endTime = time.time()
+        time_generated_row = ( endTime - startTime ) * 1000
+        country_row = [ dataCountry[ 0 ][ 'region' ], dataCountry[ 0 ][ 'name' ], hash_country_lang, time_generated_row ] 
     else:
-        endTime=time.time()
-        tiempo=(endTime-startTime)*1000
-        print(f"No se encontraron datos del pais {pais}, tiempo de búsqueda {tiempo}ms")           
-        fila=None           
-    return fila
+        endTime = time.time()
+        time_generated_row = ( endTime - startTime ) * 1000
+        print( f"No se encontraron datos del pais { country }, tiempo de búsqueda {time_generated_row}ms" )           
+        country_row = None           
+    return country_row
 
-def adicionarFila(pais,df):
+def add_countryRow_to_dataFrame( country, df ):
     '''Usando la funcion generarFilas(pais) Agrega una fila al dataframe df
     , guarda el dataframe en la base de datos y en un archivo .json'''
-    fila = generarFila(pais)
-    if fila!=None:
-        df.loc[len(df)]=fila
-        
-    # else:
-    #    print("No se encontraron datos para el pais: ", pais)
+    country_row = generate_countryRow( country )
+    if country_row!=None:
+        df.loc[ len( df ) ] = country_row
+    
     return df
-    
-    
 
 if __name__=='__main__':
     # listadePaises = ["Angola","Argentina", "Brasil", "Chile", "Colombia", "United States of America", "United K", "Uruguay", "Venezuela", "España","Valledupar","Bogota"]
-    if len(sys.argv)>1:
-        listadePaises=sys.argv[1:]
-        # print(listadePaises)
-        for pais in listadePaises:
-            df=adicionarFila(pais,df)
+    if len( sys.argv ) > 1:
+        listCountries = sys.argv[1:]
+
+        for country in listCountries:
+            df = add_countryRow_to_dataFrame( listCountries, df)
             
     
         print('_____________________________________________________________')
