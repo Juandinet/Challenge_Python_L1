e581a85f747ad9d42f8bb46a392d06b74abaf636
Juandinet <juandinet@gmail.com>
2022-06-03 20:58:32 -0500

    clean code

--- src/test_app.py
+++ src/test_app.py
@@ -6,63 +6,63 @@ df=pd.DataFrame(columns=["Region", "Country Name","Languaje", "Time(ms)"])
 # engine=create_engine('sqlite://', echo=False)
 
 class TestApp(unittest.TestCase):
-    def test_obtenerDatosdelPais_Ang(self):
+    def test_get_DataCuntry_Ang(self):
         pais = 'Angola'
-        datosPais = app.obtenerDatosdelPais(pais)
+        datosPais = app.get_DataCuntry(pais)
         self.assertEqual(datosPais[0]['region'], 'Africa')
         self.assertEqual(datosPais[0]['name'], 'Angola')
         self.assertEqual(datosPais[0]['languages'][0]['name'], 'Portuguese')
 
-    def test_obtenerDatosdelPais_Esp(self):
+    def test_get_DataCuntry_Esp(self):
         pais = 'España'
-        datosPais = app.obtenerDatosdelPais(pais)
+        datosPais = app.get_DataCuntry(pais)
         self.assertEqual(datosPais[0]['region'], 'Europe')
         self.assertEqual(datosPais[0]['name'], 'Spain')
         self.assertEqual(datosPais[0]['languages'][0]['name'], 'Spanish')
 
-    def test_obtenerDatosdelPais_Col(self):
+    def test_get_DataCuntry_Col(self):
         pais = 'Colombia'
-        datosPais = app.obtenerDatosdelPais(pais)
+        datosPais = app.get_DataCuntry(pais)
         self.assertEqual(datosPais[0]['region'], 'Americas')
         self.assertEqual(datosPais[0]['name'], 'Colombia')
         self.assertEqual(datosPais[0]['languages'][0]['name'], 'Spanish')
 
-    def test_obtenerDatosdelPais_Vall(self):
+    def test_get_DataCuntry_Vall(self):
         pais = 'Valledupar'
-        datosPais = app.obtenerDatosdelPais(pais)
+        datosPais = app.get_DataCuntry(pais)
         self.assertEqual(datosPais, None)
 
-    def test_generarFila_Esp(self):
+    def test_generate_countryRow_Esp(self):
         pais = 'España'
-        fila = app.generarFila(pais)
+        fila = app.generate_countryRow(pais)
         self.assertEqual(fila[0], 'Europe')
         self.assertEqual(fila[1], 'Spain')
         self.assertEqual(fila[2], '8df7f1b361b2af42d36011e00d22c0f9891ec0b0')
 
-    def test_generarFila_Col(self):
+    def test_generate_countryRow_Col(self):
         pais = 'Colombia'
-        fila = app.generarFila(pais)
+        fila = app.generate_countryRow(pais)
         self.assertEqual(fila[0], 'Americas')
         self.assertEqual(fila[1], 'Colombia')
         self.assertEqual(fila[2], '8df7f1b361b2af42d36011e00d22c0f9891ec0b0')    
     
     
-    def test_generarFila_Ang(self):
+    def test_generate_countryRow_Ang(self):
         pais = 'Angola'
-        fila = app.generarFila(pais)
+        fila = app.generate_countryRow(pais)
         self.assertEqual(fila[0], 'Africa')
         self.assertEqual(fila[1], 'Angola')
         self.assertEqual(fila[2], '23882c575954a0789bf02aba9e6dd01f539bc738')
 
-    def test_generarFila_Valle(self):
+    def test_generate_countryRow_Valle(self):
         pais = 'Valledupar'
-        fila = app.generarFila(pais)
+        fila = app.generate_countryRow(pais)
         self.assertEqual(fila, None)
     
-    def test_adicionarFila(self):
+    def test_add_countryRow_to_dataFrame(self):
         listadePaises = ["Angola","Argentina","España","Valledupar"]
         for pais in listadePaises:
-            app.adicionarFila(pais, df)
+            app.add_countryRow_to_dataFrame(pais, df)
         self.assertEqual(df["Region"][0], 'Africa')
         self.assertEqual(df["Country Name"][0], 'Angola')
         self.assertEqual(df["Region"][1], 'Americas')
