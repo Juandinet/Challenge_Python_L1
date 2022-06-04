df2870f0ff84faadf20940625b2cfa54e89ac45d
Juandinet <juandinet@gmail.com>
2022-06-04 11:14:10 -0500

    clean code

--- src/app.py
+++ src/app.py
@@ -7,10 +7,16 @@ import sys
 df = pd.DataFrame( columns = ["Region", "Country Name","Languaje", "Time(ms)"] )
 engine = create_engine( 'sqlite://', echo = False )
 
-def obtian_DataCuntry( country ):
-    '''Dado un pais, los cunsulta  en restcountries.com y devuelve una lista con los datos del pais'''
+def get_DataCountry( country_name ):
+    """
+    It takes a country name as a string, and returns a list of dictionaries, each dictionary containing
+    information about a country with that name
+    
+    :param country: The name of the country you want to get data from
+    :return: A list of dictionaries.
+    """
     try:
-        url = "https://restcountries.com/v2/name/" + country
+        url = "https://restcountries.com/v2/name/" + country_name
         response = requests.get( url )
         
         if response.status_code == 200:
@@ -25,10 +31,10 @@ def obtian_DataCuntry( country ):
         
     
 
-def generate_countryRow( country ):
+def generate_countryRow( country_name ):
     """Dado el nombre de un pais, devuelve una fila(lista) con los datos que requerimos del pais"""
     startTime = time.time()
-    dataCountry = obtian_DataCuntry( country )
+    dataCountry = get_DataCountry( country_name )
     if dataCountry is not None:
         country_lang = dataCountry[0]['languages'][0]['name']
         hash_country_lang = hashlib.sha1(country_lang.encode( 'utf-8' ) ).hexdigest()
@@ -42,10 +48,10 @@ def generate_countryRow( country ):
         country_row = None           
     return country_row
 
-def add_countryRow_to_dataFrame( country, df ):
+def add_countryRow_to_dataFrame( country_name, df ):
     '''Usando la funcion generarFilas(pais) Agrega una fila al dataframe df
     , guarda el dataframe en la base de datos y en un archivo .json'''
-    country_row = generate_countryRow( country )
+    country_row = generate_countryRow( country_name )
     if country_row!=None:
         df.loc[ len( df ) ] = country_row
     
